d5a61f477c584161f181b458f3957e8c736b8e0c
Kevin DeVrou <kevin.devrou@gmail.com>
2015-05-22 08:06:56 -0400

    stub out the controller methods

---
+++ app/controllers/Users.scala
@@ -0,0 +1,19 @@
+package controllers
+
+import play.api.mvc._
+
+/**
+ * Created by Kevin on 5/21/2015.
+ */
+class Users extends Controller {
+
+  def get = TODO
+
+  def get(id: Long) = TODO
+
+  def create = TODO
+
+  def update(id: Long) = TODO
+
+  def delete(id: Long) = TODO
+}
