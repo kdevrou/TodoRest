876a17145fd116ab4edc8bfbe0418b5cb91024a4
Kevin DeVrou <kdevrou@gohi5.com>
2015-05-22 20:40:15 -0400

    add evolution for user table

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,13 +1,16 @@
 # --- !Ups
 
-CREATE TABLE `usertable` (
-  `id`                     BIGINT(20)  NOT NULL AUTO_INCREMENT,
-  `email`                  VARCHAR(100) NOT NULL,
-  `name`                   VARCHAR(255) NOT NULL,
-  `password`               VARCHAR(50) NOT NULL,
-  `createdate`             DATE NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `IDX_ID` (`email`)
+CREATE TABLE usertable (
+  id                     BIGINT(20)  NOT NULL AUTO_INCREMENT,
+  email                  VARCHAR(100) NOT NULL,
+  name                   VARCHAR(255) NOT NULL,
+  password               VARCHAR(50) NOT NULL,
+  createdate             DATE DEFAULT GETDATE() NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY IDX_ID (email)
 );
 
-INSERT INTO `usertable` VALUES (1, 'jsmith@example.com', 'John Smith', 'secret123',  null, '2013-07-01');
+INSERT INTO usertable (email, name, password) VALUES ('kevin.devrou@gmail.com', 'Kevin DeVrou', 'secret123');
+
+# --- !Downs
+DROP TABLE usertable
