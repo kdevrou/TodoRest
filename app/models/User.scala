22cd8f96418428954d87474f768a4172049f884e
Kevin DeVrou <kdevrou@gohi5.com>
2015-05-22 20:41:04 -0400

    add methods to query user table

--- app/models/User.scala
+++ app/models/User.scala
@@ -1,11 +1,16 @@
 package models
 
+import java.util.Date
+
+import org.joda.time.DateTime
+import play.Logger
+
 
 /**
  * Created by Kevin on 5/21/2015.
  */
 case class User(id: Long,       // used to query
-                email: String,  // used to autenticate
+                email: String,  // used to authenticate
                 name: String,
                 password: String)
 
@@ -29,6 +34,37 @@ object User {
     )((user: User) => (
       user.id,
       user.name,
+      user.email,
       None // never put the password into json
     ))
+
+  import anorm._
+  import anorm.~
+  import anorm.SqlParser._
+  import play.api.db.DB
+  import play.api.Play.current
+
+  val parser = {
+    get[Long]("id") ~
+      get[String]("name") ~
+      get[String]("email") ~
+      get[String]("password")
+  } map {
+    case id ~ name ~ email ~ password =>
+      User(id, name, email, password)
+  }
+
+  val multiParser: ResultSetParser[Seq[User]] = parser *
+
+  def findOneById(id: Long): Option[User] = DB.withConnection { implicit connection =>
+    SQL("SELECT * FROM usertable WHERE id = {id}").on('id -> id).as(parser.singleOpt)
+  }
+
+  def findByEmailAndPassword(email: String, password: String): Option[User] = DB.withConnection {
+    Logger.debug(s"received email: $email and password: $password")
+    implicit connection =>
+      SQL("SELECT * FROM usertable WHERE email = {email} AND password = {password}").on(
+        'email -> email, 'password -> password
+      ).as(parser.singleOpt)
+  }
 }
