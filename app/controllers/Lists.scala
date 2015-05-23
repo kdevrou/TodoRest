c8a2f9d7d8cbbb9b708d9888d991f1c912d4f956
Kevin DeVrou <kdevrou@gohi5.com>
2015-05-22 20:33:45 -0400

    stub out remaining methods for routes

---
+++ app/controllers/Lists.scala
@@ -0,0 +1,19 @@
+package controllers
+
+import play.api.mvc.Controller
+
+/**
+ * Created by kdevrou on 5/22/2015.
+ */
+object Lists extends Controller {
+
+  def getAll(userId: Long) = TODO
+
+  def get(userId: Long, id: Long) = TODO
+
+  def create(userId: Long) = TODO
+
+  def update(userId: Long, id: Long) = TODO
+
+  def delete(userId: Long, id: Long) = TODO
+}
---
+++ app/controllers/Tasks.java
@@ -0,0 +1,32 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+/**
+ * Created by kdevrou on 5/22/2015.
+ */
+public class Tasks extends Controller {
+
+    public static Result getAll(Long listId) {
+        return TODO;
+    }
+
+    public static Result get(Long listId, Long id) {
+        return TODO;
+    }
+
+    public static Result create(Long listId) {
+        return TODO;
+    }
+
+    public static Result update(Long listId, Long id) {
+        return TODO;
+    }
+
+    public static Result delete(Long listId, Long id) {
+        return TODO;
+    }
+
+
+}
